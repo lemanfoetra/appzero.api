4ef14898dcb2db9d0c4c79de77a86560db8a769b
Ardyn Sulaeman <lemanfoetra@gmail.com>
2024-08-25 11:04:46 +0700

    Ubah api get base menus

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -12,25 +12,7 @@ class BaseController extends Controller
     public function menus()
     {
         try {
-            $menus = DB::table('role_menus')
-                ->select(['menus.*'])
-                ->join('menus', 'menus.id', '=', 'role_menus.id_menus')
-                ->where('role_menus.id_roles', Auth::user()->id_role)
-                ->where('menus.id_parrent', '0')
-                ->orderBy('urutan', 'asc')
-                ->get();
-
-            foreach ($menus as $key => $menu) {
-                $child = DB::table('role_menus')
-                    ->select(['menus.*'])
-                    ->join('menus', 'menus.id', '=', 'role_menus.id_menus')
-                    ->where('role_menus.id_roles', Auth::user()->id_role)
-                    ->where('menus.id_parrent', $menu->id)
-                    ->get();
-
-                $menus[$key]->child = $child;
-            }
-
+            $menus = $this->getListMenus();
             return response()->json([
                 'success'   => true,
                 'message'   => '',
@@ -43,5 +25,48 @@ class BaseController extends Controller
                 'data'      => [],
             ], 500);
         }
+    }
+
+
+    private function getListMenus()
+    {
+        $menus = [];
+        $parrents = DB::table('menus')
+            ->select(['id', 'id_parrent', 'menu', 'link', 'urutan'])
+            ->where('id_parrent', '0')
+            ->orderBy('urutan', 'asc')
+            ->get();
+
+        foreach ($parrents as $parrent) {
+            $parrent            = $parrent;
+            $parrent->childs  = $this->childMenus($parrent->id);
+            $menus = array_merge($menus, [$parrent]);
+        }
+
+        return $menus;
+    }
+
+
+    private function childMenus($id_menu)
+    {
+        $menus = [];
+        $childs = DB::table('menus')
+            ->select(['id', 'id_parrent', 'menu', 'link', 'urutan'])
+            ->where('id_parrent', $id_menu)
+            ->orderBy('urutan', 'asc')
+            ->get();
+
+        foreach ($childs as $child) {
+            $level3 = DB::table('menus')
+                ->select(['id', 'id_parrent', 'menu', 'link', 'urutan'])
+                ->where('id_parrent', $child->id)
+                ->orderBy('urutan', 'asc')
+                ->get();
+
+            $child->childs = $level3;
+            $menus = array_merge($menus, [$child]);
+        }
+
+        return $menus;
     }
 }
